8d2f581186a5490a07ec8d5622f1df0b42d23498
Svitlana D <svetk2801@gmail.com>
2023-01-13 22:00:45 +0200

    module 4 task 5 Switch

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int a;
+        if((year % 4) == 0){
+            a = 1;
+            if (year % 100 == 0){
+                if (year % 400 != 0){
+                    a = 2;
+                }
+            }
+        } else {
+            a = 2;
+        }
+
+        switch (a){
+            case (1):
+                System.out.println("leap");
+                break;
+            case (2):
+                System.out.println("not leap");
+                break;
+        }
     }
 }
